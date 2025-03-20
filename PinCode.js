6a51c99cd28e7a3216f1698df83574de83e70d08
Rhea Lather <rhea.lather_cs.da21@gla.ac.in>
2025-03-20 16:19:08 +0530

    [Rhea] ADD: Added PinCode Javascript code to the branch

--- PinCode.js
+++ PinCode.js
@@ -1,4 +1,17 @@
-function validatePinCode(pin){
-    const pinRegex=/^[1-9][0-9]{5}$/;
-    return pinRegex.test(pin);
+function validatePinCode(pin) {
+    // Regex pattern: Accepts 6 digits with optional space in between, disallows non-numeric prefixes/suffixes
+    const pinRegex = /^[0-9]{3}\s?[0-9]{3}$/;
+
+    if (pinRegex.test(pin)) {
+        console.log(`Valid PIN Code: ${pin}`);
+    } else {
+        console.log(`Invalid PIN Code: ${pin}`);
+    }
 }
+
+// Test Cases
+validatePinCode("400088");  
+validatePinCode("400 088"); 
+validatePinCode("A400088"); 
+validatePinCode("400088B"); 
+validatePinCode("4000@88"); 
